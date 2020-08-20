5754607bba990ced8c5b3fb88ac9a2da9763baba
lucashenrique <henrique.lucas025@gmail.com>
2020-08-20 09:35:05 -0300

    estruturei

--- src/main.c
+++ src/main.c
@@ -20,44 +20,39 @@ void multiprocessos(int num_proc, int *v, int tamanho) {
 	if (num_proc == 4) {
 		int aux = 0;
 		for(k=0;k<=tamanho;k=k+4) {
-					if (aux != 1) {
-						p1 = fork();
-						if(p1==0) {
-								 for (j = 2; j < v[k]; j++) {
-										if (v[k] % j == 0) {
-														resultado=1;
-														break; } }
-								 if (resultado == 0 && v[k] != 0 && v[k] != 1) { (*b) = (*b)+1; }
-								 exit(0); } 
-						waitpid(p1, NULL, 0);
+			if (aux != 1) {
+				p1 = fork();
+				if(p1==0) {	 for (j = 2; j < v[k]; j++) {
+								if (v[k] % j == 0) {	resultado=1;
+											break; } }
+						 if (resultado == 0 && v[k] != 0 && v[k] != 1) { (*b) = (*b)+1; }
+						 exit(0); } 
 
-						if(aux != 3) {
-							p2 = fork();
-							if(p2==0) {
-									 for (j = 2; j < v[k+1]; j++) {
-											if (v[k+1] % j == 0) {
-															resultado=1;
-															break; } }
-									 if (resultado == 0 && v[k+1] != 0 && v[k+1] != 1) { (*b) = (*b)+1; }
-									 exit(0); } 
-							waitpid(p2, NULL, 0);
+				if(aux != 3) {
+					p2 = fork();
+					if(p2==0) {	 for (j = 2; j < v[k+1]; j++) {
+									if (v[k+1] % j == 0) {  resultado=1;
+												break; } }
+							 if (resultado == 0 && v[k+1] != 0 && v[k+1] != 1) { (*b) = (*b)+1; }
+							 exit(0); } 
+
 							
-							if(aux != 2) {
-								p3 = fork();	
-								if(p3==0) {
-										 for (j = 2; j < v[k+2]; j++) {
-												if (v[k+2] % j == 0) {
-																resultado=1;
-																break; } }
-										 if (resultado == 0 && v[k+2] != 0 && v[k+2] != 1) { (*b) = (*b)+1; }
-										exit(0); }		
-								waitpid(p3, NULL, 0); 
+					if(aux != 2) {
+						p3 = fork();	
+						if(p3==0) {	 for (j = 2; j < v[k+2]; j++) {
+										if (v[k+2] % j == 0) {	resultado=1;
+													break; } }
+								 if (resultado == 0 && v[k+2] != 0 && v[k+2] != 1) { (*b) = (*b)+1; }
+							         exit(0); }
+
 						} } } 
 					
 					 for (j = 2; j < v[k+3]; j++) {
-							 if (v[k+3] % j == 0) {
-												  resultado=1;
-												  break; } }
+							 if (v[k+3] % j == 0) {	  resultado=1;
+										  break; } }
+				waitpid(p1, NULL, 0);
+				waitpid(p2, NULL, 0);		
+				waitpid(p3, NULL, 0); 
 				if (resultado == 0 && v[k+3] != 0 && v[k+3] != 1) { (*b) = (*b)+1; } } 
 				
 				if (tamanho - k == 3) { aux = 3; }
@@ -78,6 +73,7 @@ void multiprocessos(int num_proc, int *v, int tamanho) {
          else printf("%d\n",*b); }
 
 
+
     if (num_proc == 2) {
 
         p1 = fork();
@@ -94,13 +90,15 @@ void multiprocessos(int num_proc, int *v, int tamanho) {
                  if (v[1] % j == 0) {
                                       resultado=1;
                                       break; } }
-                 if (resultado == 0 && v[1] != 0 && v[1] != 1) { *b = *b+1; }} 
-     
+                 if (resultado == 0 && v[1] != 0 && v[1] != 1) { *b = *b+1; } }
+
+ 
+    
 
    if (num_proc == 3) {
 
         p1 = fork();
-		p2 = fork();
+	p2 = fork();
         if(p1==0) {
                  for (j = 2; j < v[0]; j++) {
                         if (v[0] % j == 0) {
@@ -117,15 +115,13 @@ void multiprocessos(int num_proc, int *v, int tamanho) {
                  exit(0); }
 				 
          waitpid(p1, NULL, 0);
-		 waitpid(p2, NULL, 0);
+	 waitpid(p2, NULL, 0);
          for (j = 2; j < v[2]; j++) {
                  if (v[2] % j == 0) {
                                       resultado=1;
                                       break; } }
-                 if (resultado == 0 && v[2] != 0 && v[2] != 1) { *b = *b+1; } }
- 
-      
-	printf("%d\n",*b); }
+                 if (resultado == 0 && v[2] != 0 && v[2] != 1) { *b = *b+1; } }   
+         printf("%d\n",*b); }
 
 
 
